26416af194bb28a1f51c3faafc1c0c3b0ebd6d19
lucas vinicius <lucassilved@gmail.com>
2023-08-14 07:18:41 -0300

    feat: add state in TransactionModal

--- components/TransactionModal/index.tsx
+++ components/TransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import exitIcon from "../../assets/exit.svg";
+import { useState } from "react";
 
 type TransactionModalProps = {
   isOpen: boolean;
@@ -7,6 +8,10 @@ type TransactionModalProps = {
 
 export function TransactionModal({ isOpen }: TransactionModalProps) {
   if (!isOpen) return null;
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [category, setCategory] = useState("");
+
   return (
     <div className="w-full h-full top-0 left-0 bg-black/70 absolute flex items-center justify-center">
       <main className="w-[480px] flex flex-col justify-center bg-gray-600 p-8 rounded-lg">
@@ -20,16 +25,22 @@ export function TransactionModal({ isOpen }: TransactionModalProps) {
           className="h-14 p-4 mb-4 text-black border rounded bg-slate-200 border-gray-300"
           type="text"
           placeholder="Descrição"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
         <input
           className="h-14 p-4 mb-4 text-black border rounded bg-slate-200 border-gray-300"
           type="text"
           placeholder="Preço"
+          value={price}
+          onChange={(e) => setPrice(Number(e.target.value))}
         />
         <input
           className="h-14 p-4 mb-4 text-black border rounded bg-slate-200 border-gray-300"
           type="text"
           placeholder="Categoria"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
         />
         <div className="w-full flex justify-between mt-2">
           <button className="w-2/5 bg-gray-500 py-4 rounded-lg hover:bg-slate-400">
