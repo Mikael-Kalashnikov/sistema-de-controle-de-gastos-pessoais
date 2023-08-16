7032a37ad4f3cdeb0f3001240366af10ef152eb3
lucas vinicius <lucassilved@gmail.com>
2023-08-16 19:17:45 -0300

    feat: implements sign up and sign in

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,32 +1,37 @@
 import { collection, getDocs } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
-const firebaseApp = initializeApp({
+const firebaseApp = {
   apiKey: "AIzaSyCy8h1VrIpOvbM-_ZCrTmLBGcsn2MJc9Hw",
   authDomain: "sistema-de-gastos-pessoais.firebaseapp.com",
   projectId: "sistema-de-gastos-pessoais",
-});
+};
 
-export const App = () =>{
-  const [name, setName] = useState("");
-  const [value, setValue] = useState("");
-  const [type, setType] = useState("");
-  const [date, setDate] = useState("");
-  const [transacao, setTransacao] = useState([]);
+let firebase_app =
+  getApps().length === 0 ? initializeApp(firebaseApp) : getApps()[0];
 
-  const db = getFirestore(firebaseApp);
-  const userCollectionRef = collection(db, "Transações");
+export default firebase_app;
 
-  useEffect(() => {
-    const getTransacao = async () =>{
-      const data = await getDocs(userCollectionRef);
-      console.log(data.docs.map((doc)=> ({...doc.data(), id:doc.id})));
-    };
-    getTransacao();
-  }, []);
-}
+// export const App = () =>{
+//   const [name, setName] = useState("");
+//   const [value, setValue] = useState("");
+//   const [type, setType] = useState("");
+//   const [date, setDate] = useState("");
+//   const [transacao, setTransacao] = useState([]);
+
+//   const db = getFirestore(firebaseApp);
+//   const userCollectionRef = collection(db, "Transações");
+
+//   useEffect(() => {
+//     const getTransacao = async () =>{
+//       const data = await getDocs(userCollectionRef);
+//       console.log(data.docs.map((doc)=> ({...doc.data(), id:doc.id})));
+//     };
+//     getTransacao();
+//   }, []);
+// }
 //firebase.initializeApp(firebaseApp);
 
 //export { fbAuth, fbRestore, fbStorage };
