7f31f99c132971cca924c0a10a2a36fe168538e1
lucas vinicius <lucassilved@gmail.com>
2023-09-10 13:30:11 -0300

    feat: add cors

--- api-financy/src/application.js
+++ api-financy/src/application.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { routes } from "./api/index.js";
 import { client } from "./infra/db.js";
 
@@ -6,6 +7,7 @@ client.connect().then(() => {
   const app = express();
   const PORT = 3001;
   app.use(express.json());
+  app.use(cors);
   app.use("/", routes);
   app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
